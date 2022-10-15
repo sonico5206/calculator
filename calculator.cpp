1c9674012a0c38d7982df3497bd0f4c199aa0f07
SONIKO <sofikulich@icloud.com>
2022-10-15 20:51:41 +0300

    calculator v1.0

--- calculator.cpp
+++ calculator.cpp
@@ -1,48 +1,46 @@
 ﻿// calculator
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-double calculator(double x, double y, char action) {
-	
+string calculator(double x, double y, char action) {
+
 	switch (action) {
 	case '+':
-		return x + y;
+		return to_string (x + y);
 		break;
 	case '-':
-		return x - y;
+		return to_string(x - y);
 		break;
 	case '*':
-		return x * y;
+		return to_string(x * y);
 		break;
 	case '/':
 		if (y != 0) {
-			return x / y;
+			return to_string(x / y);
 		}
 		else {
-			cout << "Division by 0 exception" << endl;
-		 }
+			return "error";
+		}
 		break;
 	default:
-		return 0.0;
+		return "error";
 	}
 }
 int main()
 {
 	for (int i = 0;; i++) {
-	double x;
-	double y;
-	char action;
-	string exit;
-	cout << "vvedite x=";
-	cin >>  x;
-	cout << "vvedite y=";
-	cin >> y;
-	cout << "vvedite operathiu(+,-,*,/):";
-	cin >> action;
-	cout << calculator(x, y, action)<<endl;
-	/*cout << "exit from calculator";
-	cin >> exit;
-	if (exit=='yes')*/
+		double x;
+		double y;
+		char action;
+		string exit;
+		cin >> x >> action >> y;
+		cout << calculator(x, y, action) << endl;
+		cout << "exit from calculator(yes/no):";
+		cin >> exit;
+		if (exit == "yes") {
+			return 0;
+		}
 	}
 }
